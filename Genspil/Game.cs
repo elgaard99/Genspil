ad1ded5e154912030877a8c337d2f282d686759e
Dall <d.scharla@hotmail.com>
2024-03-26 10:59:15 +0100

    Game klass m referencenumber og void metode for at lave referencenumber

--- Genspil/Game.cs
+++ Genspil/Game.cs
@@ -8,5 +8,19 @@ namespace Genspil
 {
     internal class Game
     {
+        string referenceNumber;
+//Muligvis god idé med en property?
+/*        public string ReferenceNumber 
+        { 
+            get { return referenceNumber; } 
+            set {  referenceNumber = value; }
+        }*/
+        //Laver et reference nummer ud fra title condition og en counter der ligger i gamegroup klassen
+        public void CreateReferenceNumber (string title, string condition)
+        {
+            //Kan jo så alternativt løses med property istedet.
+            this.referenceNumber = title + "-" +  gameGroup.counter + condition;
+            // Her bliver det færdige produkt eksempelvis "Matador-127A" 
+        }
     }
 }
