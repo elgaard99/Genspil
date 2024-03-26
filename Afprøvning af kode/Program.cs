36a3b077aa98e2aba0469e4145e7437490bf2377
Dall <d.scharla@hotmail.com>
2024-03-26 12:03:46 +0100

    Tilføjet CreateGame metode

---
+++ Afprøvning af kode/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Afprøvning_af_kode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] games = new string[] { "Matador", "Kalaha", "Ludo" };
+            foreach(string game in games) {
+                Console.WriteLine(game);
+            }
+            string[] games2 = new string[] { "Matador", "Kalaha", "Ludo", "Uno" };
+            foreach (string game in games2)
+            {
+                Console.WriteLine(game);
+            }
+            games = games2;
+            foreach (string game in games)
+            {
+                Console.WriteLine(game);
+            }
+            //tring[] games = new string[] { "Matador", "Kalaha", "Ludo" };
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
--- Genspil/GameGroup.cs
+++ Genspil/GameGroup.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Genspil
 {
-    internal class GameGroup
+    public class GameGroup
     {
+        string title;
+
+        int counter=0;
+
+        float price;
+        float conditionPrice;
+
+        string[] categories;
+        string[] games;
+
+
+
+        int[] numbPlayers = new int[2];
+        int[] ageRecommended = new int [2];
+        public AddGameToGameGroup(int ID)
+        {
+            this.games = 
+        }
     }
 }
