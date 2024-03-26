f657301d6520387a2c22da7179bae218041c8d0b
Dall <d.scharla@hotmail.com>
2024-03-26 13:09:25 +0100

    Indså min fejl så gik i gang med at lave warehouse klassen... smh

--- Genspil/Game.cs
+++ Genspil/Game.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Genspil
 {
-    internal class Game
+    public class Game
     {
-        string referenceNumber;
+        public string referenceNumber;
 //Muligvis god idé med en property?
 /*        public string ReferenceNumber 
         { 
@@ -19,9 +19,9 @@ namespace Genspil
         public void CreateReferenceNumber (string ID, string condition)
         {
             //"oversætter" titleID til en specifik title, eksempelvis matador. Her antager vi altså at det objekt på indeksplads ID i gamegroups, der indeholder et objekt af matador gameGroupen som indeholder et field med title
-            string title = Warehouse.gameGroups[ID].title;
+            string title = GameGroup.title;
             //"oversætter" titleID til en counter, eksempelvis matador. Her antager vi altså at det objekt på indeksplads ID i gamegroups, der indeholder et objekt af matador gameGroupen som indeholder et field med det antal spil der har været tilføjet.
-            int counter = Warehouse.gameGroups[ID].counter;
+            int counter = GameGroup.counter;
             //Kan jo så alternativt løses med properties istedet.
             this.referenceNumber = title + "-" + counter + condition;
             // Her bliver det færdige produkt eksempelvis "Matador-127A"
