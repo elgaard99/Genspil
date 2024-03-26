823666a136704dc9ba6d5c2c31aab866be265fc7
Dall <d.scharla@hotmail.com>
2024-03-26 11:34:36 +0100

    Tilføjet gameGroup klassen med en sktra variable ift vores SSD

--- Genspil/Game.cs
+++ Genspil/Game.cs
@@ -15,12 +15,16 @@ namespace Genspil
             get { return referenceNumber; } 
             set {  referenceNumber = value; }
         }*/
-        //Laver et reference nummer ud fra title condition og en counter der ligger i gamegroup klassen
-        public void CreateReferenceNumber (string title, string condition)
+        //Laver et reference nummer ud fra title og condition og en counter der ligger i gamegroup objektet som findes i Warehouse klassen
+        public void CreateReferenceNumber (string ID, string condition)
         {
-            //Kan jo så alternativt løses med property istedet.
-            this.referenceNumber = title + "-" +  gameGroup.counter + condition;
-            // Her bliver det færdige produkt eksempelvis "Matador-127A" 
+            //"oversætter" titleID til en specifik title, eksempelvis matador. Her antager vi altså at det objekt på indeksplads ID i gamegroups, der indeholder et objekt af matador gameGroupen som indeholder et field med title
+            string title = Warehouse.gameGroups[ID].title;
+            //"oversætter" titleID til en counter, eksempelvis matador. Her antager vi altså at det objekt på indeksplads ID i gamegroups, der indeholder et objekt af matador gameGroupen som indeholder et field med det antal spil der har været tilføjet.
+            int counter = Warehouse.gameGroups[ID].counter;
+            //Kan jo så alternativt løses med properties istedet.
+            this.referenceNumber = title + "-" + counter + condition;
+            // Her bliver det færdige produkt eksempelvis "Matador-127A"
         }
     }
 }
