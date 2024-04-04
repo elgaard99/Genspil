3cc007421326a9a499b2287e8bf35545f0961579
Sander Elgaard Andersen <sean62359@hotmail.edu.ucl.dk>
2024-04-04 18:17:59 +0200

    indsat Daniels Request class

--- Genspil/Request.cs
+++ Genspil/Request.cs
@@ -9,5 +9,70 @@ namespace Genspil
     internal class Request
     {
         public Gamegroup requestedGamegroup;
+
+        public Game[] ReferenceNumbers;
+
+        public Request(GameGroup gameGroup, Game[] referenceNumbers)
+        {
+            GameGroup = gameGroup;
+            ReferenceNumbers = referenceNumbers;
+        }
+
+        // metode til at søge efter spil baseret på kunde krav 
+        public void SearchGamesByRequest(int? numberOfPlayers = null, int? ageRecommended = null, string searchCategory = null)
+        {
+            Console.WriteLine("Leder efter spil baseret på kriterier...");
+            foreach (var GameGroup in GameGroups)
+            {
+                bool matchesCriteria = false;
+
+                //tjek om antal spillere macther 
+                if (numberOfPlayers != null)
+                {
+                    int minPlayers = GameGroup.playersRecommended[0];
+                    int maxPlayers = GameGroup.playersRecommended[1];
+
+                    if (minPlayers <= numberOfPlayers && maxPlayers >= numberOfPlayers)
+                    {
+                        matchesCriteria = true;
+                    }
+                }
+
+                //tjek om aldersanbefalinger matcher 
+                if (ageRecommended != null)
+                {
+                    int minAgeRecommended = GameGroup.minAgeRecommended[0];
+                    int maxAgeRecommended = GameGroup.maxAgeRecommended[1];
+
+                    if (minAgeRecommended <= ageRecommended && maxAgeRecommended >= ageRecommended)
+                    {
+                        matchesCriteria = true;
+                    }
+                    
+                }
+
+                //tjek om kategorier matcher 
+                if (searchCategory != null)
+                {
+                    foreach (string category in GameGroup.categories)
+                    {
+                        if (searchCategory == category) 
+                        {
+                            matchesCriteria = true;
+                        }
+                    }
+                }
+
+                //udskriv spillet, hvis det matcher et af kriterierne 
+                if (matchesCriteria)
+                {
+                    Console.WriteLine($"spillet '{GameGroup.Title}' møder kundens krav");
+                }
+
+                
+            }
+            
+            
+        } 
     }
 }
