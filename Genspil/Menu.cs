d0d4f98b298865f85c3fcc0b2c226ece3ee92a67
Sander Elgaard Andersen <sean62359@hotmail.edu.ucl.dk>
2024-04-10 21:07:31 +0200

    i gang med menu

---
+++ Genspil/Menu.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genspil
+{
+    internal class Menu
+    {
+
+        string title;
+        public string Title { get; set; }
+
+        string[] subMenus;
+        public string[] SubMenus { get; set; }
+
+        public Menu(string title, string[] subMenus)
+        {
+            this.title = title;
+            this.subMenus = subMenus;
+        }
+        
+        public void ShowMenu()
+        {
+            Console.Clear();
+
+            Console.WriteLine(this.title + "\n");
+
+            int counter = 1;
+            foreach (string subMenu in subMenus)
+            {
+                Console.WriteLine($"{counter}. {subMenu}");
+                counter++;
+            }
+
+            Console.Write("\n\n(Tryk menupunkt eller 0 for at afslutte) ");
+        }
+
+        public int SelectSubMenu()
+        {
+            ShowMenu();
+
+            while (true)
+            {
+
+                if (int.TryParse(Console.ReadLine(), out int menuIdx))
+                {
+                    if (menuIdx == 0)
+                        return -1;
+
+                    else if (menuIdx > 0 && menuIdx < subMenus.Length)
+                        return menuIdx -1;
+                }
+
+                else
+                { Console.WriteLine("Du skal vælge et menupunkt mellem 0 og " + subMenus.Length); }
+            }
+        }
+
+        void CostumerMenu()
+        {
+
+        }
+
+        void WareHouseMenu()
+        {
+
+        }
+
+        void Request()
+        {
+
+        }
+
+    }
+
+}
--- Genspil/Program.cs
+++ Genspil/Program.cs
@@ -16,88 +16,45 @@
                 new Gamegroup("Kalaha", new[] { 2, 6 }, new[] { 10, 12 }, new[] { "kugler", "strategi" }, 199.95F, new[] {1F, 0.9F, 0.8F, 0.7F })
             };
 
-            Warehouse warehouse = new Warehouse(gamegroups);
-            DataHandler handler = new DataHandler("gamegroupData.txt");
-            string[] requestTitles = { "Matador", "UNO", "Catan", "Kalaha" };
-            string requestTitles2 = "Matador";
-            Request request = new Request(requestTitles);
-            Request request2 = new Request(requestTitles2);
-            Game One = new Game("Matador", "A", 1);
-            Game Two = new Game("Matador", "B", 2);
-            gamegroups[0].games[0] = One;
-            gamegroups[0].games[1] = Two;
-            Game Three = new Game("Uno", "C", 1);
-            gamegroups[1].games[0] = Three;
-            /*
-                        handler.Save(warehouse.gamegroups);
-                        Gamegroup[] gamegroups_ = handler.LoadGamegroups();
+            //Warehouse warehouse = new Warehouse(gamegroups);
+            //DataHandler handler = new DataHandler("gamegroupData.txt");
+            //string[] requestTitles = { "Matador", "UNO", "Catan", "Kalaha" };
+            //string requestTitles2 = "Matador";
+            //Request request = new Request(requestTitles);
+            //Request request2 = new Request(requestTitles2);
+            //Game One = new Game("Matador", "A", 1);
+            //Game Two = new Game("Matador", "B", 2);
+            //gamegroups[0].games[0] = One;
+            //gamegroups[0].games[1] = Two;
+            //Game Three = new Game("Uno", "C", 1);
+            //gamegroups[1].games[0] = Three;
+            ///*
+            //            handler.Save(warehouse.gamegroups);
+            //            Gamegroup[] gamegroups_ = handler.LoadGamegroups();
 
-                        Warehouse warehouse1 = new Warehouse(gamegroups_);
+            //            Warehouse warehouse1 = new Warehouse(gamegroups_);
 
-                        warehouse1.PrintWarehouse();
-            */
-            //warehouse.SearchTitle(gamegroups, "Matador");
-            //request.AvailabilityNotice(gamegroups, warehouse);
-            Customer customer = new Customer();
-            customer.customerNumber = 1234;
-            //customer.requests = new[] { request };
-            customer.MakeRequest(request);
-            customer.MakeRequest(request2);
-            customer.AvailabilityNotice(warehouse);
-            Console.WriteLine(customer.ToString());
+            //            warehouse1.PrintWarehouse();
+            //*/
+            ////warehouse.SearchTitle(gamegroups, "Matador");
+            ////request.AvailabilityNotice(gamegroups, warehouse);
+            //Customer customer = new Customer();
+            //customer.customerNumber = 1234;
+            ////customer.requests = new[] { request };
+            //customer.MakeRequest(request);
+            //customer.MakeRequest(request2);
+            //customer.AvailabilityNotice(warehouse);
+            //Console.WriteLine(customer.ToString());
+
+
+            Menu mainMenu = new Menu("Vores Hovedmenu");
+            Menu searchGameMenu = new Menu("Søg efter et spil på lageret");
+            Menu costumerMenu = new Menu("Kunder");
+
+            mainMenu.ShowMenu();
+
             Console.ReadLine();
 
-            /*
-Titel:kalaha
-Kategorier:
-- Noget andet
-- Terningspil
-Spillere: 2-2
-Alders anbefaling: 0-0 år
-Pris for tilstand A: 1,2156
-Pris for tilstand B: 2,213
-Pris for tilstand C: 3,132
-Pris for tilstand D: 5,312
-Pris for tilstand E: 4,532
-
-Titel:Ludo
-Kategorier:
-- Børnevenlig
-- Terningspil
-Spillere: 2-4
-Alders anbefaling: 0-0 år
-Pris for tilstand A: 1,2423
-Pris for tilstand B: 2,13
-Pris for tilstand C: 3,132
-Pris for tilstand D: 5,312
-Pris for tilstand E: 4,532
-
-Titel:Matador
-Kategorier:
-- Brætspil
-- Strategi
-- Terningspil
-Spillere: 2-4
-Alders anbefaling: 0-0 år
-Pris for tilstand A: 1,2432
-Pris for tilstand B: 2,213
-Pris for tilstand C: 3,132
-Pris for tilstand D: 5,312
-Pris for tilstand E: 4,532
-
-Titel:uno
-Kategorier:
-- Børnevenlig
-- Kortspil
-Spillere: 2-8
-Alders anbefaling: 0-0 år
-Pris for tilstand A: 1,22
-Pris for tilstand B: 2,13
-Pris for tilstand C: 3,132
-Pris for tilstand D: 5,312
-Pris for tilstand E: 4,532
-
-*/
         }
 
     }
